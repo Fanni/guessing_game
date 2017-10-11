2bc609773c0f3b1ddebf649f1f59a9a5e0162cef
Fanni <fannisik@gmail.com>
2017-10-11 22:34:33 +0700

    first code to read user input

---
+++ target/rls/debug/deps/guessing_game-bc506dac3c7978c0.d
@@ -0,0 +1,5 @@
+/Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/target/rls/debug/deps/guessing_game-bc506dac3c7978c0.rmeta: src/main.rs
+
+/Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/target/rls/debug/deps/guessing_game-bc506dac3c7978c0.d: src/main.rs
+
+src/main.rs:
---
+++ target/rls/debug/libguessing_game.d
@@ -0,0 +1,1 @@
+/Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/target/rls/debug/libguessing_game.rmeta: /Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/src/main.rs
