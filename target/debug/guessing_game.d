bd5d4c53fab29192eeac1155585b01485bfb862c
Fanni <fannisik@gmail.com>
2017-10-14 06:59:13 +0700

    completing guess number
    - add rand crate and its documentation

---
+++ target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/target/debug/guessing_game: /Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/src/main.rs
---
+++ target/rls/debug/deps/guessing_game-2ac6f7bdb7ff4740.d
@@ -0,0 +1,5 @@
+/Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/target/rls/debug/deps/guessing_game-2ac6f7bdb7ff4740.rmeta: Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/src/main.rs
+
+/Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/target/rls/debug/deps/guessing_game-2ac6f7bdb7ff4740.d: Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/src/main.rs
+
+Users/ikhwan/Fanni/playground/maenin-rust/guessing_game/src/main.rs:
